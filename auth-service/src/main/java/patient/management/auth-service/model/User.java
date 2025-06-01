1377704884897dc13b934e21fc667cb7c533248e
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2025-06-02 00:42:41 +0300

    add Patient entity

--- auth-service/src/main/java/patient/management/auth-service/model/User.java
+++ auth-service/src/main/java/patient/management/auth-service/model/User.java
@@ -4,7 +4,37 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users")package com.example.patientservice.entity;
+
+        import jakarta.persistence.*;
+        import lombok.*;
+
+@Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+}
+
 @Getter
 @Setter
 @NoArgsConstructor
---
+++ patient-service/entity/Patient.java
@@ -0,0 +1,30 @@
+package com.example.patientservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+}
